b58035aec34d572f7b5d054cbc293973829dd641
cinos81 <cinos81@gmail.com>
2015-01-22 09:42:11 +0900

    예제 16추가

---
+++ 16/Sample1.java
@@ -0,0 +1,10 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class Sample1 extends Applet
+{
+    public void paint(Graphics g)
+    {
+        g.drawString("Java 애플릿에 오신 것을 환영합니다!", 10, 10);
+    }
+}
---
+++ 16/Sample3.java
@@ -0,0 +1,16 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.Image;
+
+public class Sample3 extends Applet
+{
+    Image img;
+    public void init()
+    {
+         img = getImage(getDocumentBase(),"Image.gif");
+    }
+    public void paint(Graphics g)
+    {
+        g.drawImage(img, 10, 10, this);
+    }
+}

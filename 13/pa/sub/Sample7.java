32538421d343ddd138b1c3f3cd1d63f63defdd60
cinos81 <cinos81@gmail.com>
2015-01-24 14:47:53 +0900

    13장 예제파일을 추가

---
+++ 13/pa/sub/Sample7.java
@@ -0,0 +1,35 @@
+package pa.sub;
+
+// 자동차 클래스
+class Car
+{
+    private int num;
+    private double gas;
+    public Car ()
+    {
+        num = 0;
+        gas = 0.0;
+        System.out.println("자동차가 만들어졌습니다.");
+    }
+    public void setCar(int n, double g)
+    {
+        num = n;
+        gas = g;
+        System.out.println("차량 번호를 " + num + " 으로, 연료 양을 " + gas + " 로 바꾸었습니다.");
+    }
+    public void show()
+    {
+        System.out.println("차량 번호는 " + num + " 입니다.");
+        System.out.println("연료 양은 " + gas + " 입니다.");
+    }
+}
+
+
+class Sample7
+{
+    public static void main(String[] args)
+    {
+        Car car1 = new Car();
+	car1.show();
+    }
+}
---
+++ 13/pb/Sample5.java
@@ -0,0 +1,10 @@
+package pb;
+
+class Sample5
+{
+    public static void main(String[] args)
+    {
+        Lesson13.Sample5.pc.Car car1 = new Lesson13.Sample5.pc.Car();
+        car1.show();
+    }
+}

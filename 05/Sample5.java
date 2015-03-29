5ca0fb78e5454a6412eff95499e81d1ac2cb0bec
cinos <cinos81@gmail.com>
2015-03-30 07:18:17 +0900

    줄바꿈

--- 05/Sample5.java
+++ 05/Sample5.java
@@ -4,19 +4,22 @@ class Sample5
 {
     public static void main(String[] args) throws IOException
     {
-        System.out.println("Á¤¼ö¸¦ ÀÔ·ÂÇÏ½Ê½Ã¿À.");
+        System.out.println("ì •ìˆ˜ë¥¼ ì…ë ¥í•˜ì‹­ì‹œì˜¤.");
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         String str = br.readLine();
         int res = Integer.parseInt(str);
+        
         switch(res){
             case 1:
-                System.out.println("1ÀÌ ÀÔ·ÂµÇ¾ú½À´Ï´Ù.");
+                System.out.println("1ì´ ì…ë ¥ë˜ì—ˆìŠµë‹ˆë‹¤.");
                 break;
             case 2 :
-                System.out.println("2°¡ ÀÔ·ÂµÇ¾ú½À´Ï´Ù.");
+                System.out.println("2ê°€ ì…ë ¥ë˜ì—ˆìŠµë‹ˆë‹¤.");
                 break;
             default :
-                System.out.println("1 È¤Àº 2¸¦ ÀÔ·ÂÇÏ½Ê½Ã¿À.");
+                System.out.println("1 í˜¹ì€ 2ë¥¼ ì…ë ¥í•˜ì‹­ì‹œì˜¤.");
                 break;
         }
     }

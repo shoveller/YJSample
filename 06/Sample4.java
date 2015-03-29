0b9b16124dee01556ff47a2c2a74f8e580974d4e
cinos <cinos81@gmail.com>
2015-03-30 07:31:24 +0900

    Update Sample4.java

--- 06/Sample4.java
+++ 06/Sample4.java
@@ -4,14 +4,18 @@ class Sample4
 {
     public static void main(String[] args) throws IOException
     {
-        System.out.println("¼ıÀÚ ¸î±îÁöÀÇ ÇÕÀ» ±¸ÇÏ½Ã°Ú½À´Ï±î?");
+        System.out.println("ìˆ«ì ëª‡ê¹Œì§€ì˜ í•©ì„ êµ¬í•˜ì‹œê² ìŠµë‹ˆê¹Œ?");
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         String str = br.readLine();
         int num = Integer.parseInt(str);
+        
         int sum = 0;
         for(int i=1; i<=num; i++){
             sum += i;
         }
-        System.out.println("1ºÎÅÍ " + num + " ±îÁöÀÇ ÇÕÀº " + sum + " ÀÔ´Ï´Ù.");
+        
+        System.out.println("1ë¶€í„° " + num + " ê¹Œì§€ì˜ í•©ì€ " + sum + " ì…ë‹ˆë‹¤.");
     }
 }

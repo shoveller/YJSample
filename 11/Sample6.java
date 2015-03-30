43671f6e60aa73437f0a888069c4a6e322b45e95
cinos <cinos81@gmail.com>
2015-03-31 05:50:45 +0900

    Update Sample6.java

--- 11/Sample6.java
+++ 11/Sample6.java
@@ -5,10 +5,13 @@ class Sample6
 
         Car[] cars;
         cars = new Car[2];
+        
         cars[0] = new Car();
         cars[0].setCar(1234, 20.5);
+        
         cars[1] = new RacingCar();
         cars[1].setCar(4567, 30.5);
+        
         for(int i=0; i< cars.length; i++){
             cars[i].show();
         }

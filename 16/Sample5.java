c41fecc5b1f1931dc595a11db2f4514e5139dad1
cinos <cinos81@gmail.com>
2015-03-31 06:22:14 +0900

    Update Sample5.java

--- 16/Sample5.java
+++ 16/Sample5.java
@@ -1,7 +1,7 @@
 import java.applet.Applet;
 import java.awt.Graphics;
-import  java.awt.event.MouseAdapter;
-import  java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Sample5 extends Applet
 {

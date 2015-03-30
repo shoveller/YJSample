99505b1ba23b24738a9112930dd3edd343b22489
cinos <cinos81@gmail.com>
2015-03-31 06:18:05 +0900

    Update Sample5.java

--- 15/Sample5.java
+++ 15/Sample5.java
@@ -1,6 +1,7 @@
 class Car extends Thread
 {
     private String name;
+    
     public Car(String nm)
     {
         name = nm;
@@ -8,7 +9,7 @@ class Car extends Thread
     public void run()
     {
         for(int i=0; i<5; i++){
-            System.out.println(name + " °¡ µ¿ÀÛÇÏ°í ÀÖ½À´Ï´Ù.");
+            System.out.println(name + " ê°€ ë™ìž‘í•˜ê³  ìžˆìŠµë‹ˆë‹¤.");
         }
     }
 }
@@ -16,12 +17,14 @@ class Sample5
 {
     public static void main(String[] args)
     {
-        Car car1 = new Car("1 È£Â÷");
+        Car car1 = new Car("1 í˜¸ì°¨");
         car1.start();
+        
         try{
             car1.join();
         }
         catch(InterruptedException  e){}
-        System.out.println("main() ¸Þ¼Òµå ½ÇÇàÁßÀÔ´Ï´Ù.");
+        
+        System.out.println("main() ë©”ì†Œë“œ ì‹¤í–‰ì¤‘ìž…ë‹ˆë‹¤.");
     }
 }

5e4a4d4913e055a14472edcfae658dfba8c589d3
cinos <cinos81@gmail.com>
2015-03-30 17:36:23 +0900

    Update Sample8.java

--- 07/Sample8.java
+++ 07/Sample8.java
@@ -7,6 +7,7 @@ class Sample8
         for(int i=0; i<test.length; i++){
             System.out.println ((i + 1) + "번째 사람의 점수는 " + test [i] + "입니다.");
         }
+        
         System.out.println ( "시험의 응시자 수는 " + test.length + "명 입니다.");
     }
 }

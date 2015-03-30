d02789f52e01897a72886e59d029ac3eb8840f4f
cinos <cinos81@gmail.com>
2015-03-30 17:37:26 +0900

    Update Sample9.java

--- 07/Sample9.java
+++ 07/Sample9.java
@@ -5,12 +5,14 @@ class Sample9
     public static void main(String[] args) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         int [] test = new int[5];
-        System.out.println (test.length + " ¸íÀÇ Á¡¼ö¸¦ ÀÔ·ÂÇÏ½Ê½Ã¿À.");
+        System.out.println (test.length + " ëª…ì˜ ì ìˆ˜ë¥¼ ì…ë ¥í•˜ì‹­ì‹œì˜¤.");
         for(int i=0; i<test.length; i++){
             String str = br.readLine ();
             test [i] = Integer.parseInt (str);
         }
+        
         for(int s=0; s<test.length-1; s++){
             for(int t=s+1; t<test.length; t++){
                 if(test[t] > test[s]){
@@ -20,8 +22,9 @@ class Sample9
                 }
             }
         }
+        
         for(int j=0; j<test.length; j++){
-            System.out.println ((j + 1) + "¹øÂ° »ç¶÷ÀÇ Á¡¼ö´Â "+ test[j] + "ÀÔ´Ï´Ù.");
+            System.out.println ((j + 1) + "ë²ˆì§¸ ì‚¬ëŒì˜ ì ìˆ˜ëŠ” "+ test[j] + "ì…ë‹ˆë‹¤.");
         }
     }
 }
